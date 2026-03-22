95ebb8d90a36e7e53de0285a8ccb61452309f51e
Rounak <rounakmohata0186@gmail.com>
2026-03-22 20:08:47 +0530

    Reviews of PR #4 fixed

--- src/components/sideNavBar.tsx
+++ src/components/sideNavBar.tsx
@@ -1,41 +1,48 @@
-import { Home, Settings, TreeDeciduous, User } from 'lucide-react';
-import React, { ReactNode } from 'react';
-import Button from './ui/button';
+import { Home, Settings, TreeDeciduous, User } from "lucide-react";
+import React, { ReactNode } from "react";
+import Button from "./ui/button";
 
 const navopts: Record<string, any>[] = [
-    {
-        label: "Home",
-        key: "home",
-        icon: <Home />
-    },
-    {
-        label: "Settings",
-        key: "settings",
-        icon: <Settings />
-    },
-    {
-        label: "Profile",
-        key: "profile",
-        icon: <User />
-    }
-]
+  {
+    label: "Home",
+    key: "home",
+    icon: <Home />,
+  },
+  {
+    label: "Settings",
+    key: "settings",
+    icon: <Settings />,
+  },
+  {
+    label: "Profile",
+    key: "profile",
+    icon: <User />,
+  },
+];
 
 const SideNavBar = () => {
-    return (
-        <div className='h-screen border-r border-(--primary) bg-(--card) w-20 flex flex-col items-center gap-4 overflow-hidden'>
-            <span className='font-bold text-(--primary) bg-background flex items-center justify-center p-5'><TreeDeciduous size={50}/></span>
-            {navopts.map((opt, index) => <NavIcon key={index} label={opt.label} icon={opt.icon} />)}
-        </div>
-    )
+  return (
+    <div className="h-screen border-r border-primary bg-card w-20 flex flex-col items-center gap-4 overflow-hidden">
+      <span className="font-bold text-primary bg-background flex items-center justify-center p-5">
+        <TreeDeciduous size={50} />
+      </span>
+      {navopts.map((opt, index) => (
+        <NavIcon key={index} label={opt.label} icon={opt.icon} />
+      ))}
+    </div>
+  );
 };
 
-const NavIcon = ({ label, icon }: { label: string, icon: ReactNode }) => {
-    return (
-        <Button variant='nav' className='bg-(--card) hover:brightness-120 flex flex-col! gap-1 items-center justify-center rounded-full! p-1 text-(--primary)'>
-            <span>{icon}</span>
-            <span className='text-[10px]'>{label}</span>
-        </Button>
-    )
-}
+const NavIcon = ({ label, icon }: { label: string; icon: ReactNode }) => {
+  return (
+    <Button
+      variant="nav"
+      className=""
+    >
+      <span>{icon}</span>
+      <span className="text-[10px]">{label}</span>
+    </Button>
+  );
+};
 
-export default SideNavBar
+export default SideNavBar;

2ea4bdba29d37449da1a0711fe40866a923e13d3
Sibsankar-de <sibsankarde910@gmail.com>
2026-04-01 21:20:18 +0530

    fix: Input tag style updated.

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -62,7 +62,7 @@ const Input = ({
   return (
     <div
       className={clsx(
-        "w-full pt-2 h-15 rounded-xl overflow-hidden",
+        "w-full h-13.5 px-5 rounded-xl overflow-hidden",
         "flex items-center relative",
         "transition-all duration-250",
         className,
@@ -74,12 +74,12 @@ const Input = ({
       <label
         htmlFor={uid}
         className={clsx(
-          "absolute top-5 left-2 ml-3 ",
-          "text-[1em] cursor-text text-neutral",
+          "absolute",
+          "cursor-text text-neutral",
           "transition-all duration-200 ",
           placeholderClass,
           (isFocused || input) &&
-            "text-[0.8em] -translate-y-5 bg-transparent text-primary ml-3 origin-left",
+            "text-sm -translate-y-3.5 bg-transparent text-primary origin-left",
         )}
       >
         {placeholder}
@@ -90,7 +90,7 @@ const Input = ({
         type={inputType}
         data-slot="input"
         className={clsx(
-          "w-full py-2 px-5 outline-none border-none bg-transparent",
+          "w-full mt-3.5 outline-none border-none bg-transparent",
         )}
         value={input}
         onChange={handleChange}
@@ -102,7 +102,11 @@ const Input = ({
       {type === "password" && (
         <button
           type="button"
-          className={clsx("cursor-pointer absolute right-4 top-5.5", (isFocused || input) && "text-primary/90 transition-all duration-300" )}
+          className={clsx(
+            "text-neutral cursor-pointer absolute right-4",
+            (isFocused || input) &&
+              "text-primary/90 transition-all duration-300",
+          )}
           onClick={handlePasswordToggle}
         >
           {!showPassword ? <Eye size={19} /> : <EyeClosed size={19} />}

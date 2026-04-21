665eea04c75a7be75fc4dd77bc9805e751b698c1
GautamM-1 <gautammaity2004@gmail.com>
2026-04-21 14:18:55 +0530

    done

--- src/components/forms/ProfileDetailsForm.tsx
+++ src/components/forms/ProfileDetailsForm.tsx
@@ -9,9 +9,9 @@ export default function Page() {
   const [step, setStep] = useState(0);
 
   return (
-    <div className="max-w-xl mx-auto p-6">
+    <>
       {step === 0 && <AcademicForm onNext={() => setStep(1)} />}
       {step === 1 && <PersonalForm onBack={() => setStep(0)} />}
-    </div>
+    </>
   );
 }
